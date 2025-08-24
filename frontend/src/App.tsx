e0e077fb059a8833389a21d8cc6668f86bc1468a
poxicatalin <pocsanjr@proton.me>
2025-08-24 11:12:57 +0300

    install tailwindcss, react-hot-toasts and react icons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Toaster } from "react-hot-toast"
+
 function App() {
   return (
     <>
-      <h1>My Shop</h1>
+      <h1 className="text-5xl">My Shop</h1>
+      <Toaster />
     </>
   )
 }
